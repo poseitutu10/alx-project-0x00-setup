ed866e1eae3e749245345c7c30577f567782b7b0
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-05-30 17:07:36 +0000

    props in nextjs typescript

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 import React from "react";
 
@@ -6,6 +7,8 @@ const Landing: React.FC = () => {
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Submit" styles="bg-green-500 text-sm px-5 py-2 rounded-sm cursor-pointer" />
+      <Button  title="Save" styles="bg-yellow-300 text-base p-2 rounded-full cursor-pointer"/>
     </div>
   )
 }
