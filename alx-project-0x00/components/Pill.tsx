3074f3d2f43b27e84ef13a941e178de751cee18b
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-05-30 09:50:12 +0000

    props in nextjs

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { PillProps } from "@/interfaces";
 
-const Pill: React.FC = () => {
+const Pill: React.FC<PillProps> = ({title}) => {
   return(
-    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">Title</p>
+    <div className=" flex justify-center  items-center border-2 border-white px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
     </div>
   )
 }
